a106812a0170afeb5a1c09146ca6ead593847b55
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-06-01 04:08:58 +0200

    Correction

--- project/tests/test_ping.py
+++ project/tests/test_ping.py
@@ -8,4 +8,4 @@ def test_ping(test_app):
     response = test_app.get("/ping")
     # Then:
     assert response.status_code == 200
-    assert response.json() == {"environment": "dev", "ping": "pong!", "testing": True}
+    assert response.json() == {"environment": "dev", "ping": "pong", "testing": True}
